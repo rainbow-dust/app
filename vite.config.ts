1446b06f1694e759a9fceaa7aa35fa69ebdf3499
huamurui <huamurui@outlook.com>
2024-01-17 16:35:45 +0800

    feat: vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 2333,
     host: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:9527',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   resolve: {
     alias: {

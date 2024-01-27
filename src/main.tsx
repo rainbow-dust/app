86a07fa5cc221aa427a98ed30db9bac41fc042c0
huamurui <huamurui@outlook.com>
2024-01-27 22:38:33 +0800

    feat: move tracker to npm package

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import FufuTracker from 'fufu-tracker'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
@@ -10,5 +11,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+const fufu = new FufuTracker({
+  appId: 'furina',
+  reportUrl: 'http://localhost:9527/ping',
+  eventsTobeRecord: ['action_scroll', 'action_click'],
+})
+setInterval(() => {
+  fufu.send()
+}, 10000)
 
 console.log('hello world', import.meta.env)
